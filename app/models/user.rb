1783519673b2a2480bbbc039e6968eeb20908ccd
高田日菜子 <inkc.4306@gmail.com>
2016-06-08 14:24:11 +0900

    add associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :nickname, presence: true
+  has_many :prototypes
 end
