ec9ed48c254e868a5ce84febc6a4834d2bf7a906
高田日菜子 <inkc.4306@gmail.com>
2016-06-08 23:57:53 +0900

    edit model from image to prototype_images

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,6 +1,6 @@
 class Prototype < ActiveRecord::Base
   belongs_to :user
-  has_many :images
-  accepts_nested_attributes_for :images
+  has_many :prototype_images
+  accepts_nested_attributes_for :prototype_images
   validates :title, :catch_copy, :concept, presence: true
 end
