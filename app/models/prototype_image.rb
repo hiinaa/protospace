27aadbe36efa853cd0116d990c9f076da98491e3
高田日菜子 <inkc.4306@gmail.com>
2016-06-10 15:30:33 +0900

    edit model of prototype_image

--- app/models/prototype_image.rb
+++ app/models/prototype_image.rb
@@ -1,5 +1,5 @@
 class PrototypeImage < ActiveRecord::Base
   belongs_to :prototype
   enum status: { main: 0, sub: 1 }
-  mount_uploader :image, ImageUploader
+  mount_uploader :image, PrototypeImageUploader
 end
