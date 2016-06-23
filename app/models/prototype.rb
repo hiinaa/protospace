2164bcc05e5eaab28f4d884014a5e23fe37eec06
高田日菜子 <inkc.4306@gmail.com>
2016-06-23 14:47:02 +0900

    defined main_image method to model of prototype

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -3,4 +3,8 @@ class Prototype < ActiveRecord::Base
   has_many :prototype_images
   accepts_nested_attributes_for :prototype_images
   validates :title, :catch_copy, :concept, presence: true
+
+  def main_image
+    prototype_images.main.first.image
+  end
 end
