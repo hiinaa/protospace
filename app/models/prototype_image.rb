d6806582a3018793c7c429a52515562e19854ad3
高田日菜子 <inkc.4306@gmail.com>
2016-06-26 21:50:14 +0900

    defined validation to prototype_images.rb

--- app/models/prototype_image.rb
+++ app/models/prototype_image.rb
@@ -2,4 +2,5 @@ class PrototypeImage < ActiveRecord::Base
   belongs_to :prototype
   enum status: [:main, :sub]
   mount_uploader :image, PrototypeImageUploader
+  validates :status, presence: true
 end
