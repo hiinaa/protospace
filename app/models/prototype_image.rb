640101efe94a96ce02713ca4ce374518e29c4933
高田日菜子 <inkc.4306@gmail.com>
2016-06-27 15:02:15 +0900

    delete validation to prototype_images.rb

--- app/models/prototype_image.rb
+++ app/models/prototype_image.rb
@@ -2,5 +2,4 @@ class PrototypeImage < ActiveRecord::Base
   belongs_to :prototype
   enum status: [:main, :sub]
   mount_uploader :image, PrototypeImageUploader
-  validates :status, presence: true
 end
