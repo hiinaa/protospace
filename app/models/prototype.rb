83d758d6baedeabcb77f469496fdc6c9f327af3f
高田日菜子 <inkc.4306@gmail.com>
2016-07-01 20:40:54 +0900

    do not create record in case no image

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,7 +1,7 @@
 class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many :prototype_images
-  accepts_nested_attributes_for :prototype_images
+  accepts_nested_attributes_for :prototype_images, reject_if: proc { |attributes| attributes['image'].blank? }
   validates :title, :catch_copy, :concept, presence: true
 
   def main_image
