6c05c6265f1a22520e9cced63514a33408263106
高田日菜子 <inkc.4306@gmail.com>
2016-07-01 21:51:48 +0900

    add dependent parameter

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,6 +1,6 @@
 class Prototype < ActiveRecord::Base
   belongs_to :user
-  has_many :prototype_images
+  has_many :prototype_images, dependent: :delete_all
   accepts_nested_attributes_for :prototype_images, reject_if: proc { |attributes| attributes['image'].blank? }
   validates :title, :catch_copy, :concept, presence: true
 
