8ed80a5031228285bc48bbcfda719dcecf5865f5
高田日菜子 <inkc.4306@gmail.com>
2016-07-03 03:05:34 +0900

    delete reject_if

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,7 +1,7 @@
 class Prototype < ActiveRecord::Base
   belongs_to :user
   has_many :prototype_images, dependent: :delete_all
-  accepts_nested_attributes_for :prototype_images, reject_if: proc { |attributes| attributes['image'].blank? }
+  accepts_nested_attributes_for :prototype_images
   validates :title, :catch_copy, :concept, presence: true
 
   def main_image
