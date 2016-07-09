6a1d4940f661c8fbc1c58f09a4b50986eb8ba94c
高田日菜子 <inkc.4306@gmail.com>
2016-07-09 09:50:26 +0900

    delete value from DB and reference source

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,6 +1,6 @@
 class Prototype < ActiveRecord::Base
   belongs_to :user
-  has_many :likes
+  has_many :likes, dependent: :destroy
   has_many :prototype_images, dependent: :delete_all
   accepts_nested_attributes_for :prototype_images
   validates :title, :catch_copy, :concept, presence: true
