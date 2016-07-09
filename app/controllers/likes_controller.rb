3e804a783ecc2c1b4975d567068d4504d58f2129
高田日菜子 <inkc.4306@gmail.com>
2016-07-09 11:46:53 +0900

    add destroy action to likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,4 +3,10 @@ class LikesController < ApplicationController
     @like = Like.create(user_id: current_user.id, prototype_id: params[:prototype_id])
     @likes = Like.where(prototype_id: params[:prototype_id])
   end
+
+  def destroy
+    @like = current_user.likes.find_by(prototype_id: params[:prototype_id])
+    @likes.destroy
+    @likes = Like.where(prototype_id: params[:prototype_id])
+  end
 end
