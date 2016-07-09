013446472d0f7a6d606128a6f2e01651f7c8b86c
高田日菜子 <inkc.4306@gmail.com>
2016-07-09 09:53:51 +0900

    defined routes of likes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
   resources :prototypes
+    resources :likes, only: [:create, :destroy]
   resources :users, only: [:show, :edit, :update]
 end
