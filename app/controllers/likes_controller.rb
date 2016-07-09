3d6cc3bc1eb2cfd1bef32208d37ea2ba59748d4e
高田日菜子 <inkc.4306@gmail.com>
2016-07-09 11:38:26 +0900

    add create action to likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,6 @@
 class LikesController < ApplicationController
+  def create
+    @like = Like.create(user_id: current_user.id, prototype_id: params[:prototype_id])
+    @likes = Like.where(prototype_id: params[:prototype_id])
+  end
 end
