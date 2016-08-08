e1d8e2281d2f5a2749160e5c20ba9d52b59b1db4
高田日菜子 <inkc.4306@gmail.com>
2016-08-08 19:07:28 +0900

    add route

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   end
   resources :popular, only: :index, module: :prototypes
   resources :comments, only: :create
+  resources :tags, only: [:index, :show]
   resources :users, only: [:show, :edit, :update]
 end
